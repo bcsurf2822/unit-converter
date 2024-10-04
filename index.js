84759fd4c44e2c8fd2977758cef9f789342d68a4
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-10-04 13:54:30 -0400

    working on shorthand for coding vals in

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
-const myData = [];
-
-function convertData(m, l, k) {
-  myData.push(m * 3.281);
-  myData.push(l * 0.264);
-  myData.push(k * 2.204);
-  console.log("My Data", myData);
-}
-
-convertData(8, 9, 2);
-
-//create converter for lenth (Meter/feet) volume(liters/gallons) mass(kilo/lbs)-- follow spec -- when user clicks convert  data populate --round down to 2 dec places
+//create converter for lenth (Meter/feet) volume(liters/gallons) mass(kilo/lbs)-- follow spec -- when user clicks convert  data populate 
 const convertButton = document.getElementById("con-btn");
 const inputNumber = document.getElementById("input-num");
+const myConversions = [];
+
+const toAppend = [`${inputNumber.value} = feet  ||  ${inputNumber.value} feet = meters`]
+
+
+function convertData(m) {
+  myConversions.push(m * 3.281);
+  myConversions.push(m * 0.264);
+  myConversions.push(m * 2.204);
+
+  for (let i = 0; i < myConversions.length; i++) {
+    console.log(myConversions[i].toFixed(3))
+  }
+}
+
 
 convertButton.addEventListener("click", function () {
-  console.log(inputNumber.value);
+  // convertData(inputNumber.value)
+  console.log(convertData(inputNumber.value))
+
 });
