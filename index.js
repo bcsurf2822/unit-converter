0513e4a8865ad88d945d4fcaaad3de121d71d0c9
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2024-10-04 11:48:03 -0400

    design is done now adding js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+//create converter for lenth (Meter/feet) volume(liters/gallons) mass(kilo/lbs)-- follow spec -- when user clicks convert  data populate --round down to 2 dec places
+
